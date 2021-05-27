6b19caa37604fc936c190b3ae5bcc1a022738783
Yann Tricot <tricot.yann@gmail.com>
2021-05-27 16:44:22 +0200

    feat: Adding documentation and refactor the code.

--- src/Concurrent/Communications.java
+++ src/Concurrent/Communications.java
@@ -8,40 +8,44 @@ import java.util.HashMap;
 
 public class Communications extends Thread{
     private final DatagramPacket data;
-    final static char CHAR_SEPARATOR = 'µ';
-    static byte[] buffer = new byte[serverConcurrent.length];
     private DatagramSocket socket;
 
+    // Variables pour le port de connexion
     final static int START_PORT = 8000;
     final static int FINAL_PORT = 60000;
     private int port=0;
+
+    // Variables pour buffer d'envoi d'un message à un client
+    final static char CHAR_SEPARATOR = 'µ';
+    static byte[] buffer = new byte[serverConcurrent.length];
 
     public Communications(DatagramPacket data) throws SocketException {
         this.data = data;
         socket = new DatagramSocket();
         HashMap<Integer,Boolean> map = Tools.TestPort.testPort(START_PORT, FINAL_PORT);
-        boolean portNonTrouve = true;
-        int testCourant = START_PORT;
+        boolean nonePort = true;
+        int actualPort = START_PORT;
         do{
-            if(map.get(testCourant)) {
-                port = testCourant;
-                socket = new DatagramSocket(testCourant);
-                portNonTrouve = false;
+            if(map.get(actualPort)) {
+                port = actualPort;
+                socket = new DatagramSocket(actualPort);
+                nonePort = false;
             }
             else
-                testCourant++;
-        }while (portNonTrouve);
-        System.out.println(port);
+                actualPort++;
+        }while (nonePort);
     }
 
     @Override
     public void run() {
         boolean onGoingCom = true;
         while (onGoingCom){
+            // On recupère le message et on traite celui-ci pour nos tests.
             String msg = new String(data.getData());
             String receivedMsg = msg.split(""+ CHAR_SEPARATOR)[0];
             System.out.println("message :" + receivedMsg);
 
+            // Si le message reçu est le bon pour mettre fin à la connexion on arrête celle-ci et notifie le client
             if(clientConcurrent.MESSAGE_END_CLIENT.equals(receivedMsg)){
                 buffer = (serverConcurrent.MESSAGE_END_SERVER + CHAR_SEPARATOR).getBytes();
                 data.setData(buffer);
@@ -52,6 +56,7 @@ public class Communications extends Thread{
                     e.printStackTrace();
                 }
             }
+            // Sinon, le message n'est pas celui pour mettre fin à la connexion et on poursuit l'échange
             else{
                 byte[] bufferMsg = (serverConcurrent.MESSAGE_SERVER + CHAR_SEPARATOR).getBytes();
                 data.setData(bufferMsg);
--- src/Concurrent/main.java
+++ src/Concurrent/main.java
@@ -4,7 +4,9 @@ import java.net.SocketException;
 
 public class main {
     public static void main(String[] args) throws SocketException {
+        // On instancie un serveur sur lequel se connecter.
         serverConcurrent server = new serverConcurrent();
+        // On appelle la fonction "start" de notre serveur pour lancer ce dernier.
         server.start();
     }
 }
